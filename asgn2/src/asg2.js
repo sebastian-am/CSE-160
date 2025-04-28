66c9e800184f7897559179af1257b3dc2cfe4939
Sebastian Morgese <sebastianmorgese@cma-100-64-26-181.resnet.ucsc.edu>
2025-04-27 22:40:16 -0700

    almost done

--- asgn2/src/asg2.js
+++ asgn2/src/asg2.js
@@ -1,16 +1,18 @@
 // Sebastian Morgese
 // smorgese@ucsc.edu
 
-// Vertex shader program
+//===============================================
+// Shaders
+//===============================================
 var VSHADER_SOURCE = `
   attribute vec4 a_Position;
   uniform mat4 u_ModelMatrix;
   uniform mat4 u_GlobalRotationMatrix;
+  uniform mat4 u_ProjectionMatrix;
   void main() {
-    gl_Position = u_GlobalRotationMatrix * u_ModelMatrix * a_Position;
+    gl_Position = u_ProjectionMatrix * u_GlobalRotationMatrix * u_ModelMatrix * a_Position;
   }`
 
-// Fragment shader program
 var FSHADER_SOURCE = `
   precision mediump float;
   uniform vec4 u_FragColor;
@@ -18,10 +20,65 @@ var FSHADER_SOURCE = `
     gl_FragColor = u_FragColor;
   }`
 
-// Global Var for WebGL context
-let canvas, gl;
-let a_Position, u_FragColor, u_Size, u_ModelMatrix; // Attribute and uniform locations
+//===============================================
+// Global Variables and Constants
+//===============================================
 
+/** WebGL Contexts and Locations **/
+/** @type {HTMLCanvasElement} */ let canvas = null;
+/** @type {WebGLRenderingContext} */ let gl = null;
+/** @type {GLint} */ let a_Position = null;
+/** @type {WebGLUniformLocation} */ let u_FragColor = null;
+/** @type {WebGLUniformLocation} */ let u_ModelMatrix = null;
+/** @type {WebGLUniformLocation} */ let u_GlobalRotationMatrix = null;
+/** @type {WebGLUniformLocation} */ let u_ProjectionMatrix = null;
+
+/** Animation Controls **/
+/** @type {number} */ let g_globalAngle = 0;
+/** @type {boolean} */ let g_AnimationOn = false;
+
+/** Mouse Control **/
+/** @type {boolean} */ let g_mouseDown = false;
+/** @type {number} */ let g_lastX = 0;
+/** @type {number} */ let g_lastY = 0;
+/** @type {number} */ let g_mouseXRotation = 0;
+/** @type {number} */ let g_mouseYRotation = 0;
+
+/** Timing Variables **/
+/** @type {number} */ let g_startTime = performance.now() / 1000;
+/** @type {number} */ let g_seconds = performance.now()/1000.0-g_startTime; 
+/** @type {number} */ let g_bobHeight = 0;
+/** @type {number} */ let g_tiltAngle = 0;
+/** @type {number[]} */ let g_waveAngles = [];
+/** @type {number} */ let g_segmentCount = 4;
+
+/** Scene Objects **/
+/** @type {Cube[]} */ let g_voxelCubes = [];
+/** @type {Matrix4} */ let g_voxelBaseMatrix = new Matrix4();
+/** @type {Cube[][]} */ let g_tentacleSegments = [];
+/** @type {number[][]} */ let g_tentacleBasePositions = [
+  [0.125, -0.375, 0.375],
+  [-0.125, -0.25, 0.5],
+  [0.25,  0.0, 0.375],
+  [-0.375, -0.125, 0.375],
+  [-0.25,  0.25, 0.375],
+  [0.125, 0.25, 0.375],
+  [-0.125, 0.0, 0.5],
+];
+
+function main() {
+  setupWebGL();
+  connectVariablesToGLSL();
+  addActionForHtmlUI();
+  setupMouseControl(); 
+  buildShapes(); // Called here to preserve performance 
+
+  // Specify the color for clearing <canvas>
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+  // Clear <canvas>
+  requestAnimationFrame(tick); // Start the animation
+}
 
 function setupWebGL() {
   // Retrieve <canvas> element
@@ -36,7 +93,7 @@ function setupWebGL() {
 
   gl.enable(gl.DEPTH_TEST);
 
-  // Enableing alpha
+  // Enabling alpha
   gl.enable(gl.BLEND);
   gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
 }
@@ -48,52 +105,45 @@ function connectVariablesToGLSL() {
     return;
   }
 
-  // Get the storage location of u_ModelMatrix
-  u_ModelMatrix = gl.getUniformLocation(gl.program, 'u_ModelMatrix');
-  if (!u_ModelMatrix) {
-    console.log('Failed to get the storage location of u_ModelMatrix');
-    return;
-  }
-
-  u_GlobalRotationMatrix = gl.getUniformLocation(gl.program, 'u_GlobalRotationMatrix');
-  if (!u_GlobalRotationMatrix) {
-    console.log('Failed to get the storage location of u_GlobalRotationMatrix');
-    return;
-  }
-
-  // Set the matrix to identity
-  var identityMatrix = new Matrix4(); 
-  gl.uniformMatrix4fv(u_ModelMatrix, false, identityMatrix.elements);
-
-  // // Get the storage location of a_Position
   a_Position = gl.getAttribLocation(gl.program, 'a_Position');
   if (a_Position < 0) {
     console.log('Failed to get the storage location of a_Position');
     return;
   }
-
-  // Get the storage location of u_FragColor
   u_FragColor = gl.getUniformLocation(gl.program, 'u_FragColor');
   if (!u_FragColor) {
     console.log('Failed to get the storage location of u_FragColor');
     return;
   }
+  u_ModelMatrix = gl.getUniformLocation(gl.program, 'u_ModelMatrix');
+  if (!u_ModelMatrix) {
+    console.log('Failed to get the storage location of u_ModelMatrix');
+    return;
+  }
+  u_GlobalRotationMatrix = gl.getUniformLocation(gl.program, 'u_GlobalRotationMatrix');
+  if (!u_GlobalRotationMatrix) {
+    console.log('Failed to get the storage location of u_GlobalRotationMatrix');
+    return;
+  }
+  u_ProjectionMatrix = gl.getUniformLocation(gl.program, 'u_ProjectionMatrix');
+  if (!u_ProjectionMatrix) {
+    console.log('Failed to get the storage location of u_ProjectionMatrix');
+    return;
+  }
+  
+  const projMatrix = new Matrix4();
+  // Set the projection matrix (orthographic): left, right, bottom, top, near, far
+  projMatrix.setOrtho(-1.5, 1.5, -1.5, 1.5, 0.01, 100);  
+  gl.uniformMatrix4fv(u_ProjectionMatrix, false, projMatrix.elements);
 
+  // Set the matrix to identity
+  var identityMatrix = new Matrix4(); 
+  gl.uniformMatrix4fv(u_ModelMatrix, false, identityMatrix.elements);
 }
 
-
-// Contants 
-const POINT = 0, TRIANGLE = 1, CIRCLE = 2;
-
-//Global Var for selected shape
-let g_selectedColor = [1.0, 1.0, 1.0, 1.0]; // Default color is white
-let g_selectedSize = 20.0; // Default size is 20
-let g_selectedShape = POINT; // Default shape is point
-let g_globalAngle = 0.0; // Global angle for rotation
-let g_yellowAngle = 0.0; // Angle for the yellow arm
-let g_magentaAngle = 0.0; // Angle for the magenta box
-let g_AnimationOn = false;
-
+//===============================================
+// User Input Functions
+//===============================================
 
 function addActionForHtmlUI() {
   // Part Slider Events
@@ -117,27 +167,6 @@ function addActionForHtmlUI() {
     g_AnimationOn = false;
   };
 }
-
-function main() {
-  setupWebGL();
-  connectVariablesToGLSL();
-  addActionForHtmlUI();
-  setupMouseControl(); 
-  createVoxelSphere(); // Create the voxel sphere (out of renderScene to preserve performance)
-
-  // Specify the color for clearing <canvas>
-  gl.clearColor(0.0, 0.0, 0.0, 1.0);
-
-  // Clear <canvas>
-  requestAnimationFrame(tick); // Start the animation
-}
-
-// Global variables for mouse
-let g_mouseDown = false;
-let g_lastX = 0;
-let g_lastY = 0;
-let g_mouseXRotation = 0;
-let g_mouseYRotation = 0;
 
 function setupMouseControl() {
   // When the mouse is pressed down, start tracking movement
@@ -167,10 +196,9 @@ function setupMouseControl() {
   };
 }
 
-
-// Performance test variables
-let g_startTime = performance.now()/1000.0; // Start time for the animation
-let g_seconds = performance.now()/1000.0-g_startTime; // Current time in seconds
+//===============================================
+// Animation and Timing
+//===============================================
 
 function tick() {
   g_seconds = performance.now()/1000.0-g_startTime; // Update the current time
@@ -180,64 +208,53 @@ function tick() {
   requestAnimationFrame(tick); // Request the next frame
 }
 
+
 function updateAnimationAngles() {
   if (g_AnimationOn) {
-    g_yellowAngle = 45*Math.sin(g_seconds); // Update the yellow arm angle
+    // g_yellowAngle = 45*Math.sin(g_seconds); // Update the yellow arm 
+    g_bobHeight = 0.06 * Math.sin(1.5 * g_seconds - 1.5);
+    g_tiltAngle = 7 * Math.sin(1.5 * g_seconds); // 7 degree tilt
+
+    g_waveAngles = [];
+    for (let i = 0; i < g_segmentCount; i++) { 
+      let angle = 7 * Math.sin(1.5 * g_seconds - i); // 7 degree wave angle
+      g_waveAngles.push(angle);
+    }
+
+  } else {
+    g_bobHeight = 0;
+    g_tiltAngle = 0;
+    g_waveAngles = [];  
   }
+}
+
+//===============================================
+// Rendering
+//===============================================
+
+function setCameraMatrix() {
+  var globalRotMat = new Matrix4();
+  globalRotMat.translate(0, 0, -5);  // Move camera back
+  globalRotMat.rotate(g_mouseYRotation, 0, 1, 0);
+  globalRotMat.rotate(g_mouseXRotation, -1, 0, 0);
+  globalRotMat.rotate(g_globalAngle, 0, 1, 0);
+  gl.uniformMatrix4fv(u_GlobalRotationMatrix, false, globalRotMat.elements);
+}
+
+function setModelBaseMatrix() {
+  let baseMatrix = new Matrix4();
+  baseMatrix.setRotate(180, 0, 1, 0);  // Rotate model to face forward
+  baseMatrix.rotate(g_tiltAngle, 1, 0, 0); // Add tilt
+  baseMatrix.translate(0, g_bobHeight, 0); // Add bobbing
+  g_voxelBaseMatrix = baseMatrix;
 }
 
 function renderScene() {
   var startTime = performance.now();
-
-  // Rotateion logic
-  var globalRotMat = new Matrix4();
-  globalRotMat.rotate(g_mouseYRotation, 0, -1, 0); // Mouse left-right -> rotate Y
-  globalRotMat.rotate(g_mouseXRotation, -1, 0, 0); // Mouse up-down -> rotate X
-  globalRotMat.rotate(g_globalAngle, 0, 1, 0);    // Plus the slider rotation
-  gl.uniformMatrix4fv(u_GlobalRotationMatrix, false, globalRotMat.elements);
-
-  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT); // Clear <canvas>
-
-
-  // Draw iris
-  // var iris = new Cylinder();
-  // iris.color = [0.2, 0.2, 0.8, 1.0];
-  // iris.segments = 16;
-  // iris.matrix.translate(0.0, 0.0, -0.501);  
-  // iris.matrix.scale(0.5, 0.5, 0.2); 
-  // iris.render();
-
-  // Draw pupil
-  var pup = new Cylinder();
-  pup.color = [0.0, 0.0, 0.0, 1.0];
-  pup.segments = 8;
-  pup.matrix.translate(0.0, 0.0, -0.52);
-  pup.matrix.scale(0.3, 0.3, 0.2);
-  pup.render();
-
-  //Draw shine
-  var shine = new Cylinder();
-  shine.color = [1.0, 1.0, 1.0, 0.3];
-  shine.segments = 16;
-  shine.matrix.translate(-0.1, 0.06, -0.521);
-  shine.matrix.scale(0.1, 0.08, 0.2);
-  shine.render();
-
-  // Draw shine 2
-  var shine2 = new Cylinder();
-  shine2.color = [1.0, 1.0, 1.0, 0.2];
-  shine2.segments = 16;
-  shine2.matrix.translate(0.01, -0.02, -0.521);
-  shine2.matrix.scale(0.075, 0.06, 0.2);
-  shine2.render();
-
-  // Parent Base
-  let baseMatrix = new Matrix4();
-  baseMatrix.setTranslate(0, 0, 0); 
-  baseMatrix.rotate(g_globalAngle, 0, 1, 0);  // <- animation based on your slider (or auto spin)
   
-  // Save parent's matrix
-  g_voxelBaseMatrix = baseMatrix;
+  setCameraMatrix(); // Set the camera matrix
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+  setModelBaseMatrix(); // Set the model base matrix
 
   // Draw voxels
   for (let i = 0; i < g_voxelCubes.length; i++) {
@@ -246,62 +263,101 @@ function renderScene() {
     cube.matrix.multiply(cube.baseMatrix);        // Apply own position
     cube.render();
   }
- 
 
-
-
-
-
-  // // Draw the body cube
-  // var body = new Cube();
-  // body.color = [1.0,0.0,0.0,1.0];
-  // body.matrix.translate(-0.25, -0.75, 0.0);
-  // body.matrix.scale(0.5, 0.3, 0.5);
-  // body.render();
+  drawPupilsandReflections();
+  drawTentacles(); 
   
-  // // Draw a left arm
-  // var leftArm = new Cube();
-  // leftArm.color = [1,1,0,1];
-  // leftArm.matrix.setTranslate(0, -0.5, 0.001);
-  // leftArm.matrix.rotate(-g_yellowAngle, 0,0,1);
-  // var yellowCoordsMat = new Matrix4(leftArm.matrix);
-  // leftArm.matrix.scale(0.25, 0.7, 0.5);
-  // leftArm.matrix.translate(-0.5,0,0);
-  // leftArm.render ();
-  
-  // // Test box
-  // var box = new Cube();
-  // box.color = [1,0,1,1];
-  // box.matrix = yellowCoordsMat;
-  // box.matrix. translate(0, 0.65, 0,0);
-  // box.matrix.rotate(g_magentaAngle,0,0,1);
-  // box.matrix.scale(0.3, 0.3, 0.3);
-  // box.matrix.translate(-0.5,0,-0.001);
-  // box.render ();
-
-  //performance test
+  // Performance test
   var duration = performance.now() - startTime;
   sendTextToHTML("ms: " + Math.floor(duration) + " fps: " + Math.floor(1000/duration), "numdot");
 }
 
-let g_voxelCubes = [];
-let g_voxelBaseMatrix = new Matrix4();
+function drawPupilsandReflections() {
+  // Draw pupils
+  var pup = new Cylinder();
+  pup.color = [0.0, 0.0, 0.0, 1.0];
+  pup.segments = 8;
+  pup.matrix = new Matrix4(g_voxelBaseMatrix); 
+  pup.matrix.translate(0.0, 0.0, -0.52);
+  pup.matrix.scale(0.3, 0.3, 0.2);
+  pup.render();
+
+  //Draw eye reflections  
+  var shine = new Cylinder();
+  shine.color = [1.0, 1.0, 1.0, 0.3];
+  shine.segments = 16;
+  shine.matrix = new Matrix4(g_voxelBaseMatrix); 
+  shine.matrix.translate(-0.1, 0.06, -0.521);
+  shine.matrix.scale(0.1, 0.08, 0.2);
+  shine.render();  
+
+  var shine2 = new Cylinder();
+  shine2.color = [1.0, 1.0, 1.0, 0.2];
+  shine2.segments = 16;
+  shine2.matrix = new Matrix4(g_voxelBaseMatrix);
+  shine2.matrix.translate(0.01, -0.02, -0.521);
+  shine2.matrix.scale(0.075, 0.06, 0.2);
+  shine2.render();
+}
+
+function drawTentacles() {
+  for (let t = 0; t < g_tentacleSegments.length; t++) {
+    let base = g_tentacleBasePositions[t];
+    let tentacle = g_tentacleSegments[t];
+  
+    let tentacleBaseMatrix = new Matrix4(g_voxelBaseMatrix);
+    tentacleBaseMatrix.translate(base[0], base[1], base[2]); // base location
+  
+    let segmentLength = 0.25; // Longer segment length
+  
+    for (let i = 0; i < tentacle.length; i++) {
+      let segment = tentacle[i];
+  
+      let segmentMatrix = new Matrix4(tentacleBaseMatrix);
+      
+      if (g_waveAngles.length > i) {
+        tentacleBaseMatrix.rotate(g_waveAngles[i], 1, 0, 0);
+      }
+
+      let scaleFactor = 0.125 * (1.0 - 0.2 * i); // Shrinks toward tip
+      segmentMatrix.scale(scaleFactor, scaleFactor, segmentLength);
+  
+      segmentMatrix.translate(0, 0, 0.5); // Move half its height forward
+  
+      segment.matrix = segmentMatrix;
+      segment.render();
+  
+      tentacleBaseMatrix.translate(0, 0, segmentLength); // Move up for next segment
+    }
+  }
+}
+
+//===============================================
+// Scene Construction
+//===============================================
+
+function buildShapes() { // Build the shapes here to preserve performance
+  g_voxelCubes = []; 
+  createVoxelSphere();
+  createRedVoxels();
+  createTentacles();
+}
 
 function createVoxelSphere() {
-  let size = 8;
-  let radius = size / 2;
-
+  const size = 8;
+  const outerRadius = size / 2;
+  const innerRadius = outerRadius * 0.8; 
   // Create a sphere of cubes 
   for (let x = 0; x < size; x++) {
     for (let y = 0; y < size; y++) {
       for (let z = 0; z < size; z++) {
-        let dx = x - (size - 1) / 2; 
-        let dy = y - (size - 1) / 2; 
-        let dz = z - (size - 1) / 2; 
-        let distance = Math.sqrt(dx * dx + dy * dy + dz * dz);
+        const dx = x - (size - 1) / 2; 
+        const dy = y - (size - 1) / 2; 
+        const dz = z - (size - 1) / 2; 
+        const distance = Math.sqrt(dx * dx + dy * dy + dz * dz);
 
-        if (distance < radius) {
-          let cube = new Cube();
+        if (distance < outerRadius && distance > innerRadius) { // Makes the sphere hollow
+          const cube = new Cube();
           if (z == 0) {
             cube.color = [0.2, 0.2, 0.8, 1.0];
           }
@@ -319,9 +375,131 @@ function createVoxelSphere() {
   }
 }
 
+function createRedVoxels() {
+  const spacing = 0.125; // cube size
+  let redVoxelPositions = [
+    // x = 0.5
+    [0.5, 0.0, 0.0],
+    [0.5, 0.0, -0.125],
+
+    // x = 0.375
+    [0.375, 0.0, 0.25],
+    [0.375, 0.125, 0.125],
+    [0.375, -0.125, 0.25],
+    [0.375, -0.25, 0.125],
+
+    // x = 0.25
+    [0.25, 0.0, 0.375],
+    [0.25, 0.125, 0.375],
+    [0.25, 0.25, 0.25],
+    [0.25, 0.375, 0.125],
+    [0.25, -0.125, 0.375],
+    [0.25, -0.25, 0.375],
+    [0.25, -0.375, 0.25],
+    [0.25, -0.5, 0.125],
+    
+    // x = 0.125
+    [0.125, 0.0, 0.5],
+    [0.125, 0.125, 0.375],
+    [0.125, 0.25, 0.375],
+    [0.125, 0.375, 0.25],
+    [0.125, -0.125, 0.5],
+    [0.125, -0.25, 0.375],
+    [0.125, -0.375, 0.375],
+    [0.125, -0.5, 0.25],
+
+    // x = 0.0
+    [0.0, 0.0, 0.5],
+    [0.0, 0.125, 0.5],
+    [0.0, 0.25, 0.375],
+    [0.0, 0.375, 0.25],
+    [0.0, 0.5, 0.125],
+    [0.0, 0.5, 0],
+    [0.0, -0.125, 0.5],
+    [0.0, -0.25, 0.5],
+    [0.0, -0.375, 0.375],
+    [0.0, -0.5, 0.25],
+    [0.0, -0.625, 0.125],
+    
+    // x = -0.125
+    [-0.125, 0.0, 0.5],
+    [-0.125, 0.125, 0.5],
+    [-0.125, 0.25, 0.375],
+    [-0.125, 0.375, 0.25],
+    [-0.125, -0.125, 0.5],
+    [-0.125, -0.25, 0.5],
+    [-0.125, -0.375, 0.375],
+    [-0.125, -0.5, 0.25],
+    [-0.125, -0.625, 0.125],
+    [-0.125, -0.625, 0.0],
+
+    // x = -0.25
+    [-0.25, 0.0, 0.5],
+    [-0.25, 0.125, 0.375],
+    [-0.25, 0.25, 0.375],
+    [-0.25, 0.375, 0.25],
+    [-0.25, 0.375, 0.125],
+    [-0.25, -0.125, 0.5],
+    [-0.25, -0.25, 0.375],
+    [-0.25, -0.375, 0.375],
+    [-0.25, -0.5, 0.25],
+
+    // x = -0.375
+    [-0.375, 0.0, 0.375],
+    [-0.375, 0.125, 0.375],
+    [-0.375, 0.25, 0.25],
+    [-0.375, 0.375, 0.125],
+    [-0.375, 0.375, 0.0],
+    [-0.375, -0.125, 0.375],
+    [-0.375, -0.25, 0.375],
+    [-0.375, -0.375, 0.25],
+    [-0.375, -0.5, 0.125],
+
+    //x = -0.5 
+    [-0.5, 0.0, 0.25],
+    [-0.5, 0.125, 0.125],
+    [-0.5, -0.125, 0.25],
+    [-0.5, -0.25, 0.25],
+    [-0.5, 0.25, -0.125],
+    [-0.5, -0.375, 0.125],
+
+    // x = -0.625
+    [-0.625, -0.25, 0.0],
+  ];
+
+  // Loop through positions for easier placement
+  for (let i = 0; i < redVoxelPositions.length; i++) {
+    let pos = redVoxelPositions[i];
+    const red = new Cube();
+    red.color = [1.0, 0.0, 0.0, 1.0];
+    red.baseMatrix = new Matrix4();
+    red.baseMatrix.translate(pos[0], pos[1], pos[2]);
+    red.baseMatrix.scale(spacing, spacing, spacing);
+    g_voxelCubes.push(red);
+  }
+}
+
+function createTentacles() {
+  g_tentacleSegments = []; // clear first
+
+  for (let t = 0; t < g_tentacleBasePositions.length; t++) {
+    const tentacle = []; // an array for this tentacle
+    for (let i = 0; i < g_segmentCount; i++) {
+      const segment = new Cube();
+      segment.color = [0.95, 0.2, 0.2, 1.0]; // Reddish 
+      tentacle.push(segment);
+    }
+
+    g_tentacleSegments.push(tentacle); // store whole tentacle
+  }
+}
+
+//===============================================
+// Utility
+//===============================================
 
 function sendTextToHTML(text, htmlID) {
-  var element = document.getElementById(htmlID);
+  const element = document.getElementById(htmlID);
   if (!element) {
     console.log("Failed to get the HTML element with id: " + htmlID);
     return;
