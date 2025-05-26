3d8cbc771e2b33e9475c2132b9c1da902ad5f78c
Sebastian Morgese <95021619+sebastian-am@users.noreply.github.com>
2025-05-25 19:01:54 -0700

    set up lighting and adj particles

---
+++ asgn4/src/cylinders.js
@@ -0,0 +1,69 @@
+// Sebastian Morgese
+// smorgese@ucsc.edu
+
+class Cylinder {
+    constructor() {
+      this.type = 'cylinder';
+      this.color = [1.0, 1.0, 1.0, 1.0]; 
+      this.segments = 10;
+      this.height = 0.2;
+      this.matrix = new Matrix4();
+    }
+  
+    render(){
+        var rgba = this.color;
+        var segments = this.segments;
+        var height = this.height;
+        var matrix = this.matrix;
+        
+        gl.uniformMatrix4fv(u_ModelMatrix, false, matrix.elements);
+        gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
+        
+        drawCylinder(segments, height);
+    }
+}
+
+function drawCylinder(segments, height) {
+    var d = 0.5;
+    var angleStep = 360 / segments;
+    let vertices = [];
+
+    for (var angle = 0; angle < 360; angle += angleStep) {
+        var angle1 = angle;
+        var angle2 = angle + angleStep;
+        var vec1 = [Math.cos(angle1 * Math.PI/180) * d, Math.sin(angle1 * Math.PI/180) * d];
+        var vec2 = [Math.cos(angle2 * Math.PI/180) * d, Math.sin(angle2 * Math.PI/180) * d];
+        var pt1Top = [vec1[0], vec1[1], height];
+        var pt2Top = [vec2[0], vec2[1], height];
+        var pt1Bot = [vec1[0], vec1[1], 0.0];
+        var pt2Bot = [vec2[0], vec2[1], 0.0];
+
+        // Top face
+        vertices.push(
+            0, 0, height,
+            pt1Top[0], pt1Top[1], pt1Top[2],
+            pt2Top[0], pt2Top[1], pt2Top[2]
+        );
+
+        // Bottom face
+        vertices.push(
+            0, 0, 0,
+            pt2Bot[0], pt2Bot[1], pt2Bot[2],
+            pt1Bot[0], pt1Bot[1], pt1Bot[2]
+        );
+
+        // Side faces (2 triangles each)
+        vertices.push(
+            pt1Bot[0], pt1Bot[1], pt1Bot[2],
+            pt2Bot[0], pt2Bot[1], pt2Bot[2],
+            pt2Top[0], pt2Top[1], pt2Top[2]
+        );
+        vertices.push(
+            pt1Bot[0], pt1Bot[1], pt1Bot[2],
+            pt2Top[0], pt2Top[1], pt2Top[2],
+            pt1Top[0], pt1Top[1], pt1Top[2]
+        );
+    }
+    drawTriangle3D(vertices);
+}
+
---
+++ asgn4/src/triangles.js
@@ -0,0 +1,164 @@
+// Sebastian Morgese
+// smorgese@ucsc.edu
+
+// Globla vars
+let triangleBuffer = null; 
+let triangle3DBuffer = null;
+let triangle3DUVBuffer = null;
+let uvBuffer = null;
+let normalBuffer = null;
+
+class Triangle {
+  constructor() {
+    this.type = 'triangle';
+    this.position = [0.0, 0.0, 0.0];
+    this.color = [1.0, 1.0, 1.0, 1.0]; 
+    this.size = 20.0;
+  }
+
+  // render this shape
+  render(){
+      var xy = this.position;
+      var rgba = this.color;
+      var size = this.size;
+      var d = this.size/200.0; // Used to scale the triangle
+      
+      // Pass the color of a point to u_FragColor variable
+      gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
+
+      // Draw
+      drawTriangle([xy[0], xy[1], xy[0] + d, xy[1], xy[0], xy[1] + d]); 
+  }
+}
+
+function drawTriangle(vertices) { 
+  var n = 3; // The number of vertices
+
+  if (triangleBuffer == null) {
+    triangleBuffer = gl.createBuffer();
+  }
+  
+  if (!triangleBuffer) {
+    console.log('Failed to create the buffer object');
+    return -1;
+  }
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, triangleBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.DYNAMIC_DRAW);
+
+  gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(a_Position);
+
+  gl.drawArrays(gl.TRIANGLES, 0, n);
+
+  gl.disableVertexAttribArray(a_Position); // Disable the assignment to a_Position variable when drawing points
+}
+
+function initTriangle() {
+  if (triangle3DBuffer == null) {
+    triangle3DBuffer = gl.createBuffer();
+  }
+  if (!triangle3DBuffer) {
+    console.log('Failed to create the buffer object');
+    return -1;
+  }
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, triangle3DBuffer);
+  gl.vertexAttribPointer(a_Position, 3, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(a_Position);
+}
+
+function initTriangleUV() {
+  if (uvBuffer == null) {
+    uvBuffer = gl.createBuffer();
+  }
+  if (!uvBuffer) {
+    console.log('Failed to create the UV buffer');
+    return -1;
+  }
+  gl.bindBuffer(gl.ARRAY_BUFFER, uvBuffer);
+  gl.vertexAttribPointer(a_UV, 2, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(a_UV);
+}
+
+function initTriangleNorm() {
+    if (normalBuffer == null) {
+      normalBuffer = gl.createBuffer();
+    }
+    if (!normalBuffer) {
+        console.log('Failed to create the normal buffer object');
+        return -1; 
+    }
+    gl.bindBuffer(gl.ARRAY_BUFFER, normalBuffer);
+    gl.vertexAttribPointer(a_Normal, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(a_Normal);
+}
+
+function drawTriangle3D(vertices) { 
+  var n = vertices.length / 3; 
+
+  initTriangle();
+
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.DYNAMIC_DRAW);
+  gl.drawArrays(gl.TRIANGLES, 0, n);
+
+  gl.disableVertexAttribArray(a_Position); // Disable the assignment to a_Position variable when drawing points
+}
+
+function drawTriangle3DUV(vertices, uv) { 
+  var n = vertices.length / 3; 
+  // Create a buffer object
+  if (triangle3DUVBuffer == null) {
+    triangle3DUVBuffer = gl.createBuffer();
+  }
+  if (!triangle3DUVBuffer) {
+    console.log('Failed to create the buffer object');
+    return -1;
+  }
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, triangle3DUVBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.DYNAMIC_DRAW);
+
+  gl.vertexAttribPointer(a_Position, 3, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(a_Position);
+
+  if (uvBuffer == null) {
+    uvBuffer = gl.createBuffer();
+  }
+  if (!uvBuffer) {
+    console.log('Failed to create the buffer object');
+    return -1;
+  }
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, uvBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(uv), gl.DYNAMIC_DRAW);
+
+  gl.vertexAttribPointer(a_UV, 2, gl.FLOAT , false, 0, 0);
+  gl.enableVertexAttribArray(a_UV);
+
+  gl.drawArrays(gl.TRIANGLES, 0, n);
+
+  gl.disableVertexAttribArray(a_Position);
+  gl.disableVertexAttribArray(a_UV);
+
+  triangle3DUVBuffer = null;
+}
+
+function drawTriangle3DUVNormal(vertices, uv, normals) { 
+  var n = vertices.length / 3; 
+
+  initTriangle();
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.DYNAMIC_DRAW);
+
+  initTriangleUV();
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(uv), gl.DYNAMIC_DRAW);
+
+  initTriangleNorm();
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(normals), gl.DYNAMIC_DRAW);
+
+  gl.drawArrays(gl.TRIANGLES, 0, n);
+
+  gl.disableVertexAttribArray(a_Position);
+  gl.disableVertexAttribArray(a_UV);
+  gl.disableVertexAttribArray(a_Normal);
+}
